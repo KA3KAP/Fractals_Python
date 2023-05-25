3a271283dc90bdbf15d0c864d9ac913d5a2a6350
Vadim <polinovadim13@ukr.net>
2023-05-25 16:21:36 +0300

    New Custom Fractals Added

--- turtle_graphic/arguments.py
+++ turtle_graphic/arguments.py
@@ -30,12 +30,24 @@ class Fractals:
                            "iterations": 10,  # TOP: 15
                            "angle": 90,
                            }
+    Cross_2 = {"axiom": "F+F+F+F",
+               "rules": {"F": "F+F-F+F+F"},
+               "iterations": 5,  # TOP: 6
+               "angle": 90,
+               "length": 6
+               }
     Unnamed_Fractal = {"axiom": "FF",
                        "rules": {"F": "+F+F--F+F"},
-                       "iterations": 6,  # TOP: 15
+                       "iterations": 5,  # TOP: 15
                        "angle": 45,
-                       "length": 2
+                       "length": 3
                        }
+    Unnamed_Fractal_Second = {"axiom": "FF",
+                              "rules": {"F": "+F+F--F+F+F+F--F+F", "X": "+F+F--F+F"},
+                              "iterations": 5,  # 5 - почка
+                              "angle": 45,
+                              "length": 8}
+
     Second_Unnamed_Fractal = {"axiom": "FF",
                               "rules": {"F": "+F+F--F+FX", "X": "--"},
                               "iterations": 4,  # TOP: 15
